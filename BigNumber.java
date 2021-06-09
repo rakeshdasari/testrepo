f2a2972feddfe564c40c8ea7ae17fda60ba8b225
Rakesh <java.yadav@gmail.com>
2021-06-09 08:10:01 +0530

    PWMGT-1245 Added arithmetic operation related files.

---
+++ BigNumber.java
@@ -0,0 +1,20 @@
+class BigNumber {
+	
+	public static void main(String args[]){
+		
+		int a = 100;
+		int b = 20;
+		
+		if(a>b){
+			System.out.println("a is big number");
+			System.out.println("Congratulations a");
+		}
+		else{
+			System.out.println("b is big number");
+			System.out.println("Congratulations b");
+		}
+		
+		
+	}
+
+}
---
+++ Division.java
@@ -0,0 +1,32 @@
+class Division {
+	
+	public static void main(String args[]){
+		
+		//double a = 10;
+		//double b = 3; 
+		//double c = a / b;
+		//System.out.println(c);
+		
+		/*
+		char a = 'B';
+		char b = 66;
+		System.out.println(b);
+		
+		
+		String a = "10";
+		String b = "20";
+		
+		System.out.println( "Addition: " + (30 + 40) );
+		*/
+		
+		int a = 10;
+		int b = 20;
+		boolean result = a > b;
+		
+		System.out.println("Result: " + result);
+		
+	}
+
+}
+
+
